3d6c6e400f9c1ce2da44477580ff796e3bde9a50
brylevm04 <brylevm04@gmail.com>
2023-11-11 18:56:33 +0300

    Use unstable and chaotic nixpkgs

--- standard/flake.nix
+++ standard/flake.nix
@@ -3,10 +3,11 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
     # Home manager
--- standard/overlays/default.nix
+++ standard/overlays/default.nix
@@ -14,8 +14,15 @@
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
   # be accessible through 'pkgs.unstable'
-  unstable-packages = final: _prev: {
-    unstable = import inputs.nixpkgs-unstable {
+  #unstable-packages = final: _prev: {
+  #  unstable = import inputs.nixpkgs-unstable {
+  #    system = final.system;
+  #    config.allowUnfree = true;
+  #  };
+  #};
+
+  chaotic-packages = final: _prev: {
+    chaotic = import inputs.nixpkgs-chaotic {
       system = final.system;
       config.allowUnfree = true;
     };
