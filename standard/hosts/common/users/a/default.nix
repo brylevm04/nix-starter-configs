5c1c7ee8c617a8b34362268ac6301661494cafaf
brylevm04 <brylevm04@gmail.com>
2023-11-11 17:08:25 +0300

    Add user configuration

---
+++ standard/hosts/common/users/a/default.nix
@@ -0,0 +1,27 @@
+{ pkgs, config, ... }:
+let ifTheyExist = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
+in
+{
+  users.mutableUsers = false;
+  users.users.misterio = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "video"
+      "audio"
+    ] ++ ifTheyExist [
+      "minecraft"
+      "network"
+      "wireshark"
+      "i2c"
+      "mysql"
+      "docker"
+      "podman"
+      "git"
+      "libvirtd"
+      "deluge"
+    ];
+
+    initialPassword = "password";
+  };
+}
