af78332c280e7938f5ae2208f7d7db633f67f6f7
brylevm04 <brylevm04@gmail.com>
2023-11-11 23:15:28 +0300

    Add some packages to home-manager

--- standard/home/a/features/desktop/common/all-in-one-for-now.nix
+++ standard/home/a/features/desktop/common/all-in-one-for-now.nix
@@ -39,8 +39,6 @@
     httpie # Better curl
     diffsitter # Better diff
     jq # JSON pretty printer and manipulator
-    trekscii # Cute startrek cli printer
-    timer # To help with my ADHD paralysis
 
     nil # Nix LSP
     nixfmt # Nix formatter
