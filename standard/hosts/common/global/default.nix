3feb287e45b294485b0d879de4e6b1280428e653
brylevm04 <brylevm04@gmail.com>
2023-11-11 17:10:46 +0300

    Add global configuration for hosts

---
+++ standard/hosts/common/global/default.nix
@@ -0,0 +1,31 @@
+# This file (and the global directory) holds config that i use on all hosts
+{ inputs, outputs, ... }: {
+  imports = [
+    ./locale.nix
+  ] ++ (builtins.attrValues outputs.nixosModules);
+
+  nixpkgs = {
+    overlays = builtins.attrValues outputs.overlays;
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+  hardware.enableRedistributableFirmware = true;
+
+  # Increase open file limit for sudoers
+  security.pam.loginLimits = [
+    {
+      domain = "@wheel";
+      item = "nofile";
+      type = "soft";
+      value = "524288";
+    }
+    {
+      domain = "@wheel";
+      item = "nofile";
+      type = "hard";
+      value = "1048576";
+    }
+  ];
+}
