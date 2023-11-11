7d1184bce658ca4a0af7ce540b79d6eb4ef6444d
brylevm04 <brylevm04@gmail.com>
2023-11-11 23:14:40 +0300

    Add some packages to home-manager

--- standard/home/a/features/desktop/common/all-in-one-for-now.nix
+++ standard/home/a/features/desktop/common/all-in-one-for-now.nix
@@ -1,5 +1,58 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ wireshark ostinato audacity deadbeef anki obsidian gwenview par2cmdline dar okular bitwarden scrcpy yt-dlp ffmpeg  ];
+  home.packages = with pkgs; [
+    wireshark
+    ostinato
+    audacity
+    deadbeef
+    anki
+    obsidian
+    gwenview
+    par2cmdline
+    dar
+    okular
+    bitwarden
+    scrcpy
+    yt-dlp
+    ffmpeg
+    aria
+    ariang
+    # ?
+    pavucontrol
+    xdragon
+    qbittorrent
+    qbittorrent-nox
+    alsa-utils
+    ranger
+
+    # ?
+    comma # Install and run programs by sticking a , before them
+    distrobox # Nice escape hatch, integrates docker images with my environment
+
+    bc # Calculator
+    bottom # System viewer
+    ncdu # TUI disk usage
+    eza # Better ls
+    ripgrep # Better grep
+    fd # Better find
+    httpie # Better curl
+    diffsitter # Better diff
+    jq # JSON pretty printer and manipulator
+    trekscii # Cute startrek cli printer
+    timer # To help with my ADHD paralysis
+
+    nil # Nix LSP
+    nixfmt # Nix formatter
+    nix-inspect # See which pkgs are in your PATH
+
+    ltex-ls # Spell checking LSP
+
+    tly # Tally counter
+
+    htop
+    htop-vim
+    zenith
+    yad
+  ];
 }
