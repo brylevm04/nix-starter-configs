b8351145ac680dd247c5764319d74cc80e3d8c7c
brylevm04 <brylevm04@gmail.com>
2023-11-12 17:35:29 +0300

    Fix error

--- standard/hosts/common/global/nix.nix
+++ standard/hosts/common/global/nix.nix
@@ -10,13 +10,5 @@
     # Add each flake input as a registry
     # To make nix3 commands consistent with the flake
     registry = (lib.mapAttrs (_: flake: { inherit flake; })) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
-    nixPath = [ "/etc/nix/path" ];
   };
-  environment.etc =
-    lib.mapAttrs'
-      (name: value: {
-        name = "nix/path/${name}";
-        value.source = value.flake;
-      })
-      config.nix.registry;
 }
