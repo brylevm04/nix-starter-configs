8f580541a44361e0e8bf17e0a6ab53b78e736f34
brylevm04 <brylevm04@gmail.com>
2023-11-12 17:33:29 +0300

    Fix $NIX_PATH

---
+++ standard/hosts/common/global/nix.nix
@@ -0,0 +1,22 @@
+{ inputs, lib, ... }:
+{
+  nix = {
+    settings = {
+      auto-optimise-store = lib.mkDefault true;
+      experimental-features = [ "nix-command" "flakes" "repl-flake" ];
+      system-features = [ "kvm" "big-parallel" "nixos-test" ];
+    };
+
+    # Add each flake input as a registry
+    # To make nix3 commands consistent with the flake
+    registry = (lib.mapAttrs (_: flake: { inherit flake; })) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+    nixPath = [ "/etc/nix/path" ];
+  };
+  environment.etc =
+    lib.mapAttrs'
+      (name: value: {
+        name = "nix/path/${name}";
+        value.source = value.flake;
+      })
+      config.nix.registry;
+}
