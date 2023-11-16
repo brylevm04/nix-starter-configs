adeb8bd3f4e4b49d96273b2ccd2456547bd14e65
brylevm04 <brylevm04@gmail.com>
2023-11-16 18:22:17 +0300

    Update perdoling settings

--- standard/hosts/common/optional/perdoling.nix
+++ standard/hosts/common/optional/perdoling.nix
@@ -21,6 +21,10 @@
     "net.ipv4.tcp_keepalive_probes" = 6;
     "net.ipv4.tcp_mtu_probing" = 1;
     "net.ipv4.tcp_sack" = 1;
+    "net.ipv4.tcp_fack" = 1;
+    "net.ipv4.tcp_low_latency" = 1;
+    "net.ipv4.tcp_workaround_signed_windows" = 1;
+    "net.ipv4.tcp_window_scaling" = 1;
     "net.core.default_qdisc" = "cake";
     "vm.max_map_count" = 2147483642;
     "vm.dirty_bytes" = (48 * 1024 * 124);
@@ -31,6 +35,17 @@
     "vm.zone_reclaim_mode" = 0;
     "vm.page_lock_unfairness" = 1;
     "vm.overcommit_memory" = 1;
+    "vm.dirty_writeback_centisecs" = 1500;
+    "fs.file-max" = 9223372036854775807;
+    "fs.inotify.max_user_watches" = 524288;
+    "kernel.sched_rt_runtime_us" = 980000;
+    "kernel.nmi_watchdog" = 0;
+    "kernel.shmmax" = 18446744073692774399;
+    "kernel.msgmax" = 33554432;
+    "kernel.msgmnb" = 33554432;
+
+    "fs.suid_dumpable" = 2;
+    "kernel.pid_max" = 999999;
   };
   systemd.tmpfiles.rules = [
     "w /sys/kernel/mm/transparent_hugepage/enabled - - - - madvise"
@@ -43,5 +58,21 @@
     "w /sys/kernel/debug/sched/min_granularity_ns - - - - 500000"
     "w /sys/kernel/debug/sched/wakeup_granularity_ns  - - - - 0"
     "w /sys/kernel/debug/sched/nr_migrate - - - - 8"
+  ];
+
+  security.pam.loginLimits = [
+    { domain = "@audio"; item = "rtprio" ; type = "-"   ; value = "99"       ; }
+    { domain = "*"; item = "nofile"; type = "-"; value = "9223372036854775807"; }
+    { domain = "*"; item = "memlock"; type = "-"; value = "unlimited"; }
+    { domain = "*"; item = "fsize"; type = "-"; value = "unlimited"; }
+    { domain = "*"; item = "data"; type = "-"; value = "unlimited"; }
+    { domain = "*"; item = "rss"; type = "-"; value = "unlimited"; }
+    { domain = "*"; item = "stack"; type = "-"; value = "unlimited"; }
+    { domain = "*"; item = "cpu"; type = "-"; value = "unlimited"; }
+    { domain = "*"; item = "nproc"; type = "-"; value = "unlimited"; }
+    { domain = "*"; item = "as"; type = "-"; value = "unlimited"; }
+    { domain = "*"; item = "locks"; type = "-"; value = "unlimited"; }
+    { domain = "*"; item = "sigpending"; type = "-"; value = "unlimited"; }
+    { domain = "*"; item = "msgqueue"; type = "-"; value = "unlimited"; }
   ];
 }
