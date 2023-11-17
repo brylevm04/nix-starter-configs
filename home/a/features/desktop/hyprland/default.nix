fdcbda012411b4f17178ae5eb7abafeb502d9459
brylevm04 <brylevm04@gmail.com>
2023-11-17 22:22:07 +0300

    Fix error

--- home/a/features/desktop/hyprland/default.nix
+++ home/a/features/desktop/hyprland/default.nix
@@ -7,30 +7,30 @@
   wayland.windowManager.hyprland = {
     enable = true;
     package = pkgs.inputs.hyprland.hyprland;
-  };
 
-  settings = {
-    input = {
-      kb_layout = "ru,us";
-      touchpad.disable_while_typing = false;
-    };
-    misc = {
-      vfr = true;
-      close_special_on_empty = true;
-      # Unfullscreen when opening something
-      new_window_takes_over_fullscreen = 2;
-    };
-    layerrule = [ "blur,waybar" ];
-
-    bind =
-      let
-        terminal = "${pkgs.xterm}/bin/xterm";
-      in
-      [
-        # Program bindings
-        "SUPER,Return,exec,${terminal}"
-      ]
-
-
+    settings = {
+      input = {
+        kb_layout = "ru,us";
+        touchpad.disable_while_typing = false;
       };
+      misc = {
+        vfr = true;
+        close_special_on_empty = true;
+        # Unfullscreen when opening something
+        new_window_takes_over_fullscreen = 2;
+      };
+      layerrule = [ "blur,waybar" ];
+
+      bind =
+        let
+          terminal = "${pkgs.xterm}/bin/xterm";
+        in
+        [
+          # Program bindings
+          "SUPER,Return,exec,${terminal}"
+        ]
+
+
+        };
+    };
   }
