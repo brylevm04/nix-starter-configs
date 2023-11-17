eb74db3113493de5af9ed96c7f4709558ee9f29c
brylevm04 <brylevm04@gmail.com>
2023-11-17 18:03:08 +0300

    Add flake shell

---
+++ shell/flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Flake utils demo";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-chaotic.url = "https://flakehub.com/f/chaotic-cx/nyx/*.tar.gz";
+    hardware.url = "github:nixos/nixos-hardware";
+    impermanence.url = "github:nix-community/impermanence";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+    nur = { url = "github:nix-community/NUR"; };
+  };
+
+
+  outputs = inputs: flake-utils.lib.eachDefaultSystem (system:
+    let
+      imports = [ inputs.nixpkgs-chaotic.nixosModules.default ];
+      pkgs = import nixpkgs {
+        inherit system overlays imports;
+      };
+    in
+    with pkgs;
+    {
+      devShells.default = mkShell {
+        NIX_CONFIG = "extra-experimental-features = nix-command flakes repl-flake";
+        buildInputs = [
+          nix
+          home-manager
+          git
+
+          sops
+          ssh-to-age
+          gnupg
+          age
+
+          linuxPackages_cachyos
+        ];
+      };
+    }
+  );
+}
