49ea05518e402d3bb2d5fe032c1c3e7e27c9d80d
brylevm04 <brylevm04@gmail.com>
2023-11-17 22:46:47 +0300

    Delete obsolte cfg

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -34,8 +34,6 @@
 
   services.xserver.enable = true;
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.videoDrivers = [ "qxl" ];
-  services.spice-vdagentd.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
   boot = {
