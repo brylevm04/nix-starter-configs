5af4ea83bd9baae6928259897303e5bfefdf3b28
brylevm04 <brylevm04@gmail.com>
2023-11-19 16:07:23 +0300

    Use hyprland

--- home/a/features/desktop/hyprland/default.nix
+++ home/a/features/desktop/hyprland/default.nix
@@ -9,20 +9,12 @@
   wayland.windowManager.hyprland = {
     enable = true;
     package = pkgs.inputs.hyprland.hyprland;
-
+    recommendedEnvironment = true;
     settings = {
       input = {
         kb_layout = "ru,us";
         touchpad.disable_while_typing = false;
       };
-      misc = {
-        vfr = true;
-        close_special_on_empty = true;
-        # Unfullscreen when opening something
-        new_window_takes_over_fullscreen = 2;
-      };
-      layerrule = [ "blur,waybar" ];
-
       bind =
         let
           terminal = "${pkgs.xterm}/bin/xterm";
@@ -31,8 +23,6 @@
           # Program bindings
           "SUPER,Return,exec,${terminal}"
         ];
-
-
     };
   };
 }
