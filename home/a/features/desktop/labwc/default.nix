6fc933e344950ebdb9db2566c29883e11d73b59b
brylevm04 <brylevm04@gmail.com>
2023-11-19 16:51:42 +0300

    Add new labwc configuration

---
+++ home/a/features/desktop/labwc/default.nix
@@ -0,0 +1,22 @@
+{ lib, config, pkgs, ... }:
+
+{
+  imports = [
+    ../common
+  ];
+
+  home.file = {
+    ".fvwm/config" = {
+      source = ../../../dotfiles/fvwm3/config;
+    };
+  };
+
+  home.packages = with pkgs; [
+    labwc
+    foot
+    zellij
+    kanshi
+    fuzzel
+    wlay
+  ];
+}
