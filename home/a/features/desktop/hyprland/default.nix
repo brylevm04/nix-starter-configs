cdfad0aed7f129273ececae2d7db5159b7b79021
brylevm04 <brylevm04@gmail.com>
2023-11-19 16:23:49 +0300

    Update hyprland configuration

--- home/a/features/desktop/hyprland/default.nix
+++ home/a/features/desktop/hyprland/default.nix
@@ -12,6 +12,7 @@
     settings = {
       input = {
         kb_layout = "ru,us";
+        kb_options = "grp:alt_shift_toggle";
         touchpad.disable_while_typing = false;
       };
       bind =
@@ -24,4 +25,6 @@
         ];
     };
   };
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
 }
