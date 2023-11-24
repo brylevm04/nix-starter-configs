213e5e5afc7100278c362eae395b0e65054df58a
brylevm04 <brylevm04@gmail.com>
2023-11-24 19:10:47 +0300

    Add DNS configuration

---
+++ hosts/common/optional/dns/default.nix
@@ -0,0 +1,78 @@
+{ pkgs, ... }:
+
+{
+  networking = {
+    nameservers = [ "127.0.0.1" "::1" ];
+    # If using dhcpcd:
+    dhcpcd.extraConfig = "nohook resolv.conf";
+    # If using NetworkManager:
+    networkmanager.dns = "none";
+  };
+
+  services.dnscrypt-proxy2 = {
+    enable = true;
+    settings = {
+      ipv4_servers = true;
+      ipv6_servers = false;
+      block_ipv6 = true;
+      require_dnssec = true;
+      require_nofilter = true;
+      require_nolog = true;
+      http3 = true;
+      cache = true;
+      dnscrypt_servers = true;
+      odoh_servers = true;
+      doh_servers = true;
+      anonymized_dns = {
+        routes = [
+          {
+            server_name = "dnscrypt.ca-1";
+            via = [ "*" ];
+          }
+        ];
+      };
+
+      sources = {
+        public-resolvers = {
+          urls = [
+            "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+            "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+          ];
+          cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        };
+        relays = {
+          urls = [
+            "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/relays.md"
+            "https://download.dnscrypt.info/resolvers-list/v3/relays.md"
+          ];
+          cache_file = "/var/lib/dnscrypt-proxy2/relays.md";
+          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        };
+        odoh-servers = {
+          urls = [
+            "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/odoh-servers.md"
+            "https://download.dnscrypt.info/resolvers-list/v3/odoh-servers.md"
+          ];
+          cache_file = "/var/lib/dnscrypt-proxy2/odoh-servers.md";
+          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        };
+        odoh-relays = {
+          urls = [
+            "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/odoh-relays.md"
+            "https://download.dnscrypt.info/resolvers-list/v3/odoh-relays.md"
+          ];
+          cache_file = "/var/lib/dnscrypt-proxy2/odoh-relays.md";
+          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        };
+      };
+
+      # You can choose a specific set of servers from https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
+      server_names = [ "dnscrypt.ca-1" ];
+    };
+  };
+
+  systemd.services.dnscrypt-proxy2.serviceConfig = {
+    StateDirectory = "dnscrypt-proxy";
+  };
+}
