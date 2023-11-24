fe8ac30a2b88edcb79c0f94d21893a6fff728ffa
brylevm04 <brylevm04@gmail.com>
2023-11-24 16:40:55 +0300

    Try to fix KDE

--- home/a/features/desktop/kde/default.nix
+++ home/a/features/desktop/kde/default.nix
@@ -2,14 +2,16 @@
 
 {
   imports = [
+    inputs.plasma-manager.homeManagerModules.plasma-manager
+
     ../common
   ];
   programs.plasma = {
     enable = true;
 
-    workspace.clickItemTo = "select";
+    #workspace.clickItemTo = "select";
 
-    configFile."baloofilerc"."Basic Settings"."Indexing-Enabled" = false;
+    #configFile."baloofilerc"."Basic Settings"."Indexing-Enabled" = false;
   };
 
 }
