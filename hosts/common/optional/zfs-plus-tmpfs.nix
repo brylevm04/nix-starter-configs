f4d498a274d3d4d842cac7375c66233b31748760
brylevm04 <brylevm04@gmail.com>
2023-11-25 15:00:22 +0300

    Add new supported filesystems

--- hosts/common/optional/zfs-plus-tmpfs.nix
+++ hosts/common/optional/zfs-plus-tmpfs.nix
@@ -9,7 +9,7 @@ in
     initrd = {
       availableKernelModules = [ "aesni_intel" "cryptd" "uas" "usbcore" "usb_storage" "vfat" "nls_cp437" "nls_iso8859_1" ];
     };
-    supportedFilesystems = [ "ntfs" "btrfs" "ext4" "ext3" "xfs" "vfat" "zfs" ];
+    supportedFilesystems = [ "ntfs" "btrfs" "ext4" "ext3" "xfs" "vfat" "zfs" "ntfs3" ];
     kernelParams = [ "elevator=none" ];
     loader = {
       grub = {
