31fa1beb48c9fd5526d8f0d783552620c2afb30d
brylevm04 <brylevm04@gmail.com>
2023-11-25 14:56:50 +0300

    Add new performance sysctl tweaks

--- hosts/common/optional/sysctl/desktop/performance.nix
+++ hosts/common/optional/sysctl/desktop/performance.nix
@@ -1,5 +1,11 @@
 {
   boot.kernel.sysctl = {
+    "net.netfilter.nf_conntrack_generic_timeout" = 60;
+    "net.netfilter.nf_conntrack_tcp_timeout_established" = 600;
+    "net.netfilter.nf_conntrack_tcp_timeout_time_wait" = 10;
+    "net.netfilter.nf_conntrack_max" = 1048576;
+
+    "net.core.netdev_max_backlog" = 100000;
     "net.ipv4.tcp_ecn" = 0;
     "kernel.perf_event_paranoid" = 0;
   };
